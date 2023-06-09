239ad0a6901055eca18b615d6ba39084d014cf7f
ZDibLO <xsawery.ksp@gmail.com>
2023-06-09 16:09:44 +0200

    Make Module addon compile with proper arg names

--- source/xstore/content/client/xeon/xdk/v1/shared/ModuleAddon.java
+++ source/xstore/content/client/xeon/xdk/v1/shared/ModuleAddon.java
@@ -2,6 +2,8 @@ package xstore.content.client.xeon.xdk.v1.shared;
 
 import xstore.content.client.xeon.xdk.v1.hook.EventMotionHook;
 import xstore.content.client.xeon.xdk.v1.hook.EventPacketHook;
+import xstore.content.client.xeon.xdk.v1.hook.packet.Packet;
+import xstore.content.client.xeon.xdk.v1.util.Ptr;
 
 public abstract class ModuleAddon implements Addon, EventMotionHook, EventPacketHook{
 	public final String name;
@@ -12,6 +14,12 @@ public abstract class ModuleAddon implements Addon, EventMotionHook, EventPacket
 		this.name = name;
 		this.category = category;
 	}
+	
+	@Override
+	public void invokeOnMotion(Ptr<Double> posX, Ptr<Double> posY, Ptr<Double> posZ, Ptr<Float> yaw, Ptr<Float> pitch, Ptr<Boolean> onGround, Ptr<Boolean> cancelled) {}
+	
+	@Override
+	public void invokeOnPacket(Ptr<Packet> packet, Ptr<Boolean> cancelled) {}
 	
 	public abstract void onEnable();
 	public abstract void onDisable();
