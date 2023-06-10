bdaafa5e650db5e3eade0b5d492892d376ab1a62
ZDibLO <xsawery.ksp@gmail.com>
2023-06-10 22:28:54 +0200

    Last finalest update

--- source/xstore/content/client/xeon/xdk/v1/shared/Addon.java
+++ source/xstore/content/client/xeon/xdk/v1/shared/Addon.java
@@ -2,6 +2,6 @@ package xstore.content.client.xeon.xdk.v1.shared;
 
 public interface Addon {
 	public String name();
-	public short[] version();
+	public int version();
 	public AddonKind kind();
 }
